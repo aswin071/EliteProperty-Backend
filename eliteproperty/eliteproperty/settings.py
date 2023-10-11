27bb6114450062b0f83ababa8f0fbe1136ae06de
Aswin <aswinjikz071@gmail.com>
2023-10-11 18:08:01 +0530

    hosting updations

--- eliteproperty/eliteproperty/settings.py
+++ eliteproperty/eliteproperty/settings.py
@@ -159,6 +159,12 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES ={
+    "default" :dj_database_url.parse(config("DATABASE_URL"))
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
